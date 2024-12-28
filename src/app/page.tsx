659c2260f8a4eedeff67bb78beee12e0445ac35f
Juan José Rodriguez <zoranbow@gmail.com>
2024-12-28 17:15:47 -0500

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+import Link from 'next/link';
+
 export default function HomePage() {
   return (
-    <main>
-      <h1>HOME_PAGE</h1>
+    <main className="h-full flex items-center justify-center">
+      <div className="w-full max-w-xs border-4 border-double rounded-lg border-foreground p-6 flex flex-col items-center justify-center gap-4">
+        <h1 className="text-3xl font-bold">ECOS</h1>
+        <Link
+          className="w-full border-4 border-double rounded-md border-foreground p-2 text-center hover:border-background hover:bg-foreground hover:text-background hover:font-bold"
+          href="/login"
+        >
+          Login
+        </Link>
+        <Link
+          className="w-full border-4 border-double rounded-md border-foreground p-2 text-center hover:border-background hover:bg-foreground hover:text-background hover:font-bold"
+          href="/signup"
+        >
+          Signup
+        </Link>
+      </div>
     </main>
   );
 }
