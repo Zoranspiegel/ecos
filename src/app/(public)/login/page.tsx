c8a567fb5b7a83bef9b7073aa2cf633f06581864
Juan José Rodriguez <zoranbow@gmail.com>
2024-12-28 17:19:21 -0500

    login & signup pages

---
+++ src/app/(public)/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+  return (
+    <main className='h-full flex items-center justify-center'>
+      <h1>LOGIN_PAGE</h1>
+    </main>
+  );
+}
---
+++ src/app/(public)/signup/page.tsx
@@ -0,0 +1,7 @@
+export default function SignupPage() {
+  return (
+    <main className='h-full flex items-center justify-center'>
+      <h1>SIGNUP_PAGE</h1>
+    </main>
+  );
+}
