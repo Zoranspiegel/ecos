a1d8833fc859c66623f47dee9a30790d03e22120
Juan José Rodriguez <zoranbow@gmail.com>
2025-01-05 19:58:02 -0500

    Header frontend structure

--- src/app/(private)/Header.tsx
+++ src/app/(private)/Header.tsx
@@ -1,5 +1,11 @@
 export default function Header() {
   return (
-    <header className='border-4 border-double'>HEADER</header>
+    <header className='border-4 border-double px-6 flex items-center justify-between'>
+      <h1 className='text-3xl'>Ecos</h1>
+      <div className='h-full flex items-center justify-center gap-2'>
+        <span>USERNAME</span>
+        <div className='h-[90%] aspect-square border-2 rounded-full flex items-center justify-center'>X</div>
+      </div>
+    </header>
   );
 }
