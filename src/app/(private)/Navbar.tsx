8196f5dc048499933cd3487b72a17c2947450d32
Juan José Rodriguez <zoranbow@gmail.com>
2025-01-05 22:18:45 -0500

    Navbar active links

--- src/app/(private)/Navbar.tsx
+++ src/app/(private)/Navbar.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import {
   FaHome,
   FaComment,
@@ -5,24 +7,27 @@ import {
   FaUserFriends,
   FaCog
 } from 'react-icons/fa';
+import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 
 export default function Navbar() {
+  const pathname = usePathname();  
+
   return (
     <nav className="border-4 border-double flex items-center justify-evenly">
-      <Link href="/feed" className='text-2xl'>
+      <Link href="/feed" className={`${pathname.startsWith('/feed') ? 'text-redhaus' : undefined} text-2xl`}>
         <FaHome />
       </Link>
-      <Link href="/post" className='text-xl'>
+      <Link href="/post" className={`${pathname.startsWith('/post') ? 'text-redhaus' : undefined} text-xl`}>
         <FaComment />
       </Link>
-      <Link href="/followers" className='text-2xl'>
+      <Link href="/followers" className={`${pathname.startsWith('/followers') ? 'text-redhaus' : undefined} text-2xl`}>
         <FaUserFriends />
       </Link>
-      <Link href="/following" className='text-2xl'>
+      <Link href="/following" className={`${pathname.startsWith('/following') ? 'text-redhaus' : undefined} text-2xl`}>
         <FaUsers />
       </Link>
-      <Link href="/admin" className='text-xl'>
+      <Link href="/admin" className={`${pathname.startsWith('/admin') ? 'text-redhaus' : undefined} text-xl`}>
         <FaCog />
       </Link>
     </nav>
