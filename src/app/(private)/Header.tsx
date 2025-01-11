6b78b613e2f7636808312eb0a19d719a672083e3
Juan José Rodriguez <zoranbow@gmail.com>
2025-01-11 00:47:37 -0500

    bauhaus background svgs

--- src/app/(private)/Header.tsx
+++ src/app/(private)/Header.tsx
@@ -3,7 +3,7 @@ import HeaderUser from '@/components/HeaderUser';
 export default function Header() {
   return (
     <header className='border-4 border-double border-foreground bg-background rounded-r-full pl-6 flex items-center justify-between'>
-      <h1 className='text-5xl'>Ecos</h1>
+      <h1 className='text-5xl'>ECOS</h1>
       <HeaderUser />
     </header>
   );
