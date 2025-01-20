78ce9d1f95d1432cf01bc478115286cfbc93e62b
Juan José Rodriguez <zoranbow@gmail.com>
2025-01-19 20:26:12 -0500

    /api/posts/user/:userID endpoint fix

--- src/hooks/usePosts.ts
+++ src/hooks/usePosts.ts
@@ -3,7 +3,7 @@ import { fetcher } from '@/utils/fetcher';
 import useSWR from 'swr';
 
 function getUrl({ page, content, userID }: { page: number, content?: string, userID?: string }) {
-  if (userID) return `/api/posts/${userID}?page=${page}`;
+  if (userID) return `/api/posts/user/${userID}?page=${page}`;
   if (content) return `/api/posts/search?content=${content}&page=${page}`;
   return `/api/posts/feed?page=${page}`;
 }
