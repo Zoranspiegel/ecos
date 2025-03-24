ba69d784dbee46880e2536c67c60834313743f37
Juan José Rodriguez <zoranbow@gmail.com>
2025-03-24 10:56:00 -0500

    upload image resized with sharp

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,36 +1,31 @@
-import { getClient } from '@/db';
-import env from '@/env';
-import { jwtVerify } from 'jose';
-import { NextRequest, NextResponse } from 'next/server';
-import { v2 as cloudinary } from 'cloudinary';
+import { getClient } from "@/db";
+import env from "@/env";
+import { jwtVerify } from "jose";
+import { NextRequest, NextResponse } from "next/server";
+import { v2 as cloudinary, UploadApiResponse } from "cloudinary";
+import sharp from "sharp";
 
 cloudinary.config({
   cloud_name: env.CLOUDINARY_CLOUD_NAME,
   api_key: env.CLOUDINARY_API_KEY,
-  api_secret: env.CLOUDINARY_API_SECRET
+  api_secret: env.CLOUDINARY_API_SECRET,
 });
 
 export async function POST(request: NextRequest): Promise<NextResponse> {
+  // GET CONTENT FROM BODY
+  const { content, image } = await request.json();
+
+  if (!content && !image) {
+    return NextResponse.json({ error: "Invalid input data" }, { status: 400 });
+  }
+
   try {
-    // GET CONTENT FROM BODY
-    const { content, image } = await request.json();
-
-    if (!content && !image) {
-      return NextResponse.json(
-        { error: 'Invalid input data' },
-        { status: 400 }
-      )
-    }
-
     // JWT ON COOKIE CHECK
     const cookieStore = request.cookies;
-    const cookie = cookieStore.get('jwt-token');
+    const cookie = cookieStore.get("jwt-token");
 
     if (!cookie?.value) {
-      return NextResponse.json(
-        { error: 'Unauthenticated' },
-        { status: 403 }
-      );
+      return NextResponse.json({ error: "Unauthenticated" }, { status: 403 });
     }
 
     // JWT TOKEN JOSE VERIFICATION
@@ -40,10 +35,64 @@ export async function POST(request: NextRequest): Promise<NextResponse> {
     const { payload } = await jwtVerify(token, jwtSecret);
 
     if (!payload.sub) {
-      return NextResponse.json(
-        { error: 'Unauthenticated' },
-        { status: 403 }
-      );
+      return NextResponse.json({ error: "Unauthenticated" }, { status: 403 });
+    }
+
+    interface NewImage {
+      url?: string;
+      width?: number;
+      height?: number;
+    }
+    const newImage: NewImage = {};
+
+    if (image) {
+      // TRANSFORM IMAGE FILE DATA URL TO BUFFER
+      const imageBuffer = Buffer.from(image.file.split(",")[1], "base64");
+
+      // COMPRESS & RESIZE IMAGE IF NEEDED USING SHARP LIBRARY
+      const resizedImageBuffer = await sharp(imageBuffer)
+        .resize({
+          width: 2000,
+          height: 2000,
+          fit: "inside",
+          withoutEnlargement: true,
+        })
+        .toFormat("jpeg", { quality: 80 })
+        .toBuffer();
+
+      // UPLOAD SHARP RETURNED BUFFER TO CLOUDINARY
+      try {
+        const cloudinaryResult: UploadApiResponse = await new Promise(
+          (resolve, reject) => {
+            const uploadStream = cloudinary.uploader.upload_stream(
+              {
+                public_id: image.name,
+                folder: "Ecos/Images",
+              },
+              (err, result) => {
+                if (err) reject(err);
+                else resolve(result as UploadApiResponse);
+              }
+            );
+
+            uploadStream.end(resizedImageBuffer); // BUFFER SENT
+          }
+        );
+
+        newImage.url = cloudinaryResult.secure_url;
+        newImage.width = cloudinaryResult.width;
+        newImage.height = cloudinaryResult.height;
+      } catch (error) {
+        if (error instanceof Error) {
+          console.error(error.stack);
+        } else {
+          console.error(error);
+        }
+        return NextResponse.json(
+          { error: "Internal Server Error" },
+          { status: 500 }
+        );
+      }
     }
 
     // CREATE NEW POST
@@ -54,45 +103,16 @@ export async function POST(request: NextRequest): Promise<NextResponse> {
     const newPostRes = await client.query(
       `INSERT INTO PUBLIC.posts (content, user_id) VALUES ($1, $2)
       RETURNING *`,
-      [content.normalize('NFC'), id]
-    );    
-    
+      [content.normalize("NFC"), id]
+    );
+
     if (!newPostRes.rowCount) {
       await client.end();
-      throw new Error('New Post Creation Failed');
+      throw new Error("New Post Creation Failed");
     }
 
-    // SAVE POST IMAGE
-    if (image) {
-      interface NewImage {
-        url?: string;
-        width?: number;
-        height?: number;
-      }
-      const newImage: NewImage = {};
-
-      // CLOUDINARY IMAGE STORING
-      try {
-        const result = await cloudinary.uploader.upload(image.file, {
-          public_id: image.name,
-          folder: 'Ecos/Images'
-        });
-
-        newImage.url = result.secure_url;
-        newImage.width = result.width;
-        newImage.height = result.height;
-      } catch (error) {
-        await client.end();
-        if (error instanceof Error) {
-          console.error(error.stack);
-          return NextResponse.json(
-            { error: 'Internal Server Error' },
-            { status: 500 }
-          );
-        }
-      }
-
-      // DATABASE IMAGE STORING
+    // DATABASE IMAGE STORING
+    if (newImage.url) {
       await client.query(
         `INSERT INTO PUBLIC.images (post_id, url, width, height)
         VALUES ($1, $2, $3, $4)`,
@@ -101,17 +121,17 @@ export async function POST(request: NextRequest): Promise<NextResponse> {
     }
 
     await client.end();
-    
-    return NextResponse.json(
-      { msg: 'Success' },
-      { status: 201 }
-    );
+
+    return NextResponse.json({ msg: "Success" }, { status: 201 });
   } catch (error) {
     if (error instanceof Error) {
       console.error(error.stack);
     }
+    if (image) {
+      await cloudinary.uploader.destroy(`Ecos/Images/${image.name}`);
+    }
     return NextResponse.json(
-      { error: 'Internal Server Error' },
+      { error: "Internal Server Error" },
       { status: 500 }
     );
   }
